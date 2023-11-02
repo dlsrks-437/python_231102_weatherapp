1a1c155af60035d520abb513ba86fc306a4d8a3f
dlsrks <paulhjkim3@gmail.com>
2023-11-02 12:39:19 +0900

    현재 기온 추출

--- weather_crawling.py
+++ weather_crawling.py
@@ -6,8 +6,15 @@ import requests
 
 from bs4 import BeautifulSoup
 
+area = '한남동'
 
-weather_html = requests.get('https://search.naver.com/search.naver?query=날씨')
+weather_html = requests.get(f'https://search.naver.com/search.naver?query={area} 날씨')
 print(weather_html.text)
+
+weather_soup = BeautifulSoup(weather_html.text, 'html.parser')
+
+today_temp = weather_soup.find('div', {'class' : 'temperature_text'}).text
+today_temp = today_temp[6:11]
+print(today_temp)
 
 
