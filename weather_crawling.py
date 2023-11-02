d203853e871d0dd5e5fa7bd39f844aa04e0e3e6b
dlsrks <paulhjkim3@gmail.com>
2023-11-02 14:34:30 +0900

    원하는 지역의 날씨정보(한남동만 됨..)

--- weather_crawling.py
+++ weather_crawling.py
@@ -6,21 +6,55 @@ import requests
 
 from bs4 import BeautifulSoup
 
-area = '한남동'
+# area = '한남동'
+
+area = input('날씨를 알려드립니다 : ')
 
 weather_html = requests.get(f'https://search.naver.com/search.naver?query={area} 날씨')
-print(weather_html.text)
+# print(weather_html.text)
 
 weather_soup = BeautifulSoup(weather_html.text, 'html.parser')
 
+
 today_temp = weather_soup.find('div', {'class' : 'temperature_text'}).text
 today_temp = today_temp[6:11]
-print(today_temp)
+print(today_temp)  # 현재 온도
+
 
 area_text = weather_soup.find('h2', {'class' : 'title'}).text
-print(area_text)
+print(area_text)  # 장소 지명
+
 
 weather_text = weather_soup.find('span', {'class' : 'weather before_slash'}).text
-print(weather_text)
+print(weather_text)  # 날씨
+
+
+yester_wether = weather_soup.find('p', {'class' : "summary"}).text
+yester_wether = yester_wether[:13].strip()
+print(yester_wether)  # 어제 대비 온도변화
+
+
+temp_feel1 = weather_soup.select('dl.summary_list>dd')
+#<dl>중에서 클래스가 summary_list인 태그를 찾은 후 그 안의 <dd>태그들을 모두 리스트로 반환
+temp_feel1_text = temp_feel1[0].text
+print(temp_feel1)
+
+temp_feel2 = weather_soup.find('div', {'class' : "sort"}).text
+temp_feel2 = temp_feel2.strip()
+print(temp_feel2)  # 체감온도
+
+
+dust_info = weather_soup.select('ul.today_chart_list>li')
+dust1_info = dust_info[0].find('span', {'class' : "txt"}).text
+print(dust1_info)  # 미세먼지
+dust2_info = dust_info[1].find('span', {'class' : "txt"}).text
+print(dust1_info)  # 초미세먼지
+
+part_matter = weather_soup.find('span', {'class' : "txt"}).text
+print(part_matter)  # 미세먼지
+
+
+
+
 
 
